e53dfcecd665c8ad8c61ba90947b8efe552fc479
ChallenAi <fengyiai@gmail.com>
2024-08-19 08:24:51 +0000

    doc: add doc to drawable.ts

--- src/physical/drawable.ts
+++ src/physical/drawable.ts
@@ -1,9 +1,14 @@
+/**
+ * TODO: is it necessary to add x, y to Mesh?
+ */
+
 // mesh is the basic render unit
 export interface Mesh {
   path: string;
   seed?: number;
 };
 
+// mesh style options
 export interface MeshOptions {
   renderer?: string;
   stroke?: string;
@@ -11,13 +16,14 @@ export interface MeshOptions {
   rotation?: number;
 }
 
-// Text is all the text shown
+// Text is all the text shown in our art
 export interface Text {
   x: number;
   y: number;
   content: string;
 };
 
+// text style options
 export interface TextOptions {
   maxWidth?: number;
   color?: string;
