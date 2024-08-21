8e1c06a27d035175c198420035bec1c45ed681b5
ChallenAi <fengyiai@gmail.com>
2024-08-21 08:56:57 +0000

    doc: describe shadow element structure

--- src/logical/element.ts
+++ src/logical/element.ts
@@ -1,11 +1,20 @@
 import type { Mesh, Text } from "@physical/drawable";
 
+// ShadowElement is the basic unit which stores shapes and texts
+// it contains a children property so that it is organized as a tree
 export interface ShadowElement {
+  // unique id of current element
   id: string;
+  // offset x
   x: number;
+  // offset y
   y: number;
+  // shapes of current element
   shapes?: Mesh[];
-  text?: Text[];
+  // texts of current element
+  texts?: Text[];
+  // children list of current element
   children?: ShadowElement[];
-  contains?: (x: number, y: number) => void;
+  // if current element contains the given coordination
+  contain?: (x: number, y: number) => void;
 };
