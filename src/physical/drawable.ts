d03dea470f04cf176bc6607ffbd28ee7f968a469
ChallenAi <fengyiai@gmail.com>
2024-08-21 08:49:46 +0000

    feat: add offset to mesh

--- src/physical/drawable.ts
+++ src/physical/drawable.ts
@@ -4,29 +4,46 @@
 
 // mesh is the basic render unit
 export interface Mesh {
+  // path of the given mesh
   path: string;
+  // prevent random generated shape from changing
   seed?: number;
+  // offset x
+  x?: number;
+  // offset y
+  y?: number;
 };
 
 // mesh style options
 export interface MeshOptions {
+  // selected randerer
   renderer?: string;
+  // canvas stroke of given mesh
   stroke?: string;
+  // fill color or pattern into mesh
   fill?: string;
+  // rotation in degrees
   rotation?: number;
 }
 
 // Text is all the text shown in our art
 export interface Text {
-  x: number;
-  y: number;
+  // text content
   content: string;
+  // offset x
+  x?: number;
+  // offset y
+  y?: number;
 };
 
 // text style options
 export interface TextOptions {
+  // max width of given text, the extra ones whould be throw
   maxWidth?: number;
+  // color of text
   color?: string;
+  // size of text
   size?: number;
+  // font family of text
   family?: string;
 }
