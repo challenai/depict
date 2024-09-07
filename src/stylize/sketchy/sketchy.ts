708dc31f1344f3b32c0da63beb5ad87175209422
ChallenAi <fengyiai@gmail.com>
2024-09-07 07:29:50 +0000

    feat: create a new sketchy renderer

---
+++ src/stylize/sketchy/sketchy.ts
@@ -0,0 +1,51 @@
+import type { Mesh, MeshOptions, Text, TextOptions } from "@physical/drawable";
+import { Renderer } from "@physical/render";
+
+export interface SketchyOptions {
+  defaultMeshOpts?: MeshOptions;
+  defaultTextOpts?: TextOptions;
+}
+
+const DMO: MeshOptions = {
+  stroke: "#000",
+  fill: undefined,
+  rotation: undefined,
+};
+
+const DTO: TextOptions = {
+  maxWidth: undefined,
+  color: "#000",
+  size: 14,
+  family: undefined
+};
+
+// sketchy renderer provide sketchy style wire frame to draw
+export class SketchyRenderer extends Renderer {
+  ctx: CanvasRenderingContext2D;
+  dmo: MeshOptions;
+  dto: TextOptions;
+
+  constructor(ctx: CanvasRenderingContext2D, opts: SketchyOptions) {
+    super();
+    this.ctx = ctx;
+    this.dmo = opts.defaultMeshOpts || DMO;
+    this.dto = opts.defaultTextOpts || DTO;
+  };
+
+  draw(mesh: Mesh) {
+    // TODO
+  };
+
+  // TODO: sketchy style text + default fonts?
+  write(text: Text) {
+    // set text offset
+    const x = text.x || 0;
+    const y = text.y || 0;
+    // write text
+    this.ctx.fillText(text.content, x, y);
+  };
+
+  offset(x: number, y: number) {
+    this.ctx.translate(x, y);
+  };
+}
