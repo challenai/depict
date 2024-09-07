7d4332a30aee31e9e85a2ea205f4510a72f8b047
ChallenAi <fengyiai@gmail.com>
2024-09-07 07:21:27 +0000

    doc: comment Renderer

--- src/physical/render.ts
+++ src/physical/render.ts
@@ -1,7 +1,11 @@
 import type { Mesh, Text } from './drawable';
 
+// Renderer render meshes and write texts
 export abstract class Renderer {
+  // draw meshes
   abstract draw(shape: Mesh): void
+  // write texts
   abstract write(text: Text): void
+  // move current draw cursor to given coordinates
   abstract offset(x: number, y: number): void
 };
