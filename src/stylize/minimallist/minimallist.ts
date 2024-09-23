4d5ac401420660ca3f912f57f377df4dd16eaa31
ChallenAi <fengyiai@gmail.com>
2024-09-23 08:28:47 +0000

    feat: adapt minimallist renderer to context

--- src/stylize/minimallist/minimallist.ts
+++ src/stylize/minimallist/minimallist.ts
@@ -1,80 +1,55 @@
-import type { Mesh, MeshOptions, Text, TextOptions } from "@physical/drawable";
+import type { MeshContextBuilder, TextContextBuilder } from "@physical/context";
+import type { Mesh, Text } from "@physical/drawable";
 import { Renderer } from "@physical/render";
 
 export interface MinimalistOptions {
-  defaultMeshOpts?: MeshOptions;
-  defaultTextOpts?: TextOptions;
+  meshContextBuilder: MeshContextBuilder;
+  textContextBuilder: TextContextBuilder;
 }
-
-const DMO: MeshOptions = {
-  stroke: "#000",
-  fill: undefined,
-  rotation: undefined,
-};
-
-const DTO: TextOptions = {
-  maxWidth: undefined,
-  color: "#000",
-  size: 14,
-  family: undefined
-};
 
 // minimalist renderer provide basic wire frame to draw,
 // it will draw our shapes as fast as possible.
 export class MinimalistRenderer extends Renderer {
-  dmo: MeshOptions;
-  dto: TextOptions;
+  mcb: MeshContextBuilder;
+  tcb: TextContextBuilder;
 
   constructor(opts: MinimalistOptions) {
     super();
-    this.dmo = opts.defaultMeshOpts || DMO;
-    this.dto = opts.defaultTextOpts || DTO;
+    this.mcb = opts.meshContextBuilder;
+    this.tcb = opts.textContextBuilder;
   };
 
   draw(ctx: CanvasRenderingContext2D, mesh: Mesh) {
     ctx.save();
-    const opts = mesh.opts;
+    if (mesh.opts) this.mcb(ctx, mesh.opts);
 
     // set current mesh offset
     const x = mesh.x || 0;
     const y = mesh.y || 0;
-    if (x != 0 && y != 0) ctx.translate(x, y);
+    if (x != 0 || y != 0) ctx.translate(x, y);
 
     // create 2D path
     const p2d = new Path2D(mesh.path);
+    // border: default value == true
+    if (!mesh.opts || mesh.opts?.border != false || mesh.opts.stroke) ctx.stroke(p2d);
 
-    // set stroke style
-    if (opts && opts.stroke) {
-      ctx.strokeStyle = opts.stroke;
-    } else {
-      ctx.strokeStyle = "#000";
-    }
-
-    ctx.stroke(p2d);
-
-    // fill the mesh
-    if (opts && opts.fill) {
-      ctx.fillStyle = opts.fill;
-      ctx.fill(p2d);
-    }
+    // fill the mesh, default value == false
+    if (mesh.opts?.background || mesh.opts?.fill) ctx.fill(p2d);
 
     ctx.restore();
   };
 
   write(ctx: CanvasRenderingContext2D, text: Text) {
     ctx.save();
-    const opts = text.opts || {};
+    if (text.opts) this.tcb(ctx, text.opts);
 
-    // set text offset
-    const x = text.x || 0;
-    const y = text.y || 0;
-    // write text
-    ctx.fillStyle = opts.color || (DTO.color as string);
-    const sz = opts.size || DTO.size;
-    const family = opts.family || DTO.family;
-    ctx.font = `${sz}pt ${family}`;
+    const maxWidth = text.opts?.maxWidth || undefined;
 
-    ctx.fillText(text.content, x, y);
+    // border: default value == false
+    if (text.opts?.border) ctx.strokeText(text.content, text.x || 0, text.y || 0, maxWidth);
+
+    // fill the text, default value == true
+    if (!text.opts || text.opts.background !== false || text.opts.fill) ctx.fillText(text.content, text.x || 0, text.y || 0, maxWidth);
 
     ctx.restore();
   };
