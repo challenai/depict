4f481840f69e223d305300e24b2dd3242dd84b94
ChallenAi <fengyiai@gmail.com>
2024-12-14 20:07:31 +0000

    feat: design depict

--- src/depict.ts
+++ src/depict.ts
@@ -4,22 +4,33 @@ export interface DepictOptions {
   height: number;
 };
 
+// TODO: listen resize event
+// TODO: support dynamic layers?
+// TODO: init
 export class Depict {
+  // root element to hold graph
   root: HTMLElement;
+  // canvas layers
   layers: HTMLCanvasElement[];
+  // dom posistion of current graph
   x: number;
   y: number;
+  // minimum event trigger interval
+  mei: number;
 
   constructor({
     root,
     width,
     height,
-  }: GraphOptions) {
+  }: DepictOptions) {
     this.root = root;
     this.layers = [];
+    this.mei = 16;
 
     const rect = this.root.getClientRects().item(0);
     this.x = rect ? rect.x : 0;
     this.y = rect ? rect.y : 0;
   }
+
+  run() { }
 }
