0375d1c215bbd9d84d020fe3aa035742b26e6250
ChallenAi <fengyiai@gmail.com>
2024-12-30 20:18:10 +0000

    feat: initialize svelte example by vite.js

---
+++ examples/svelte-graph/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+  import Graph from "./lib/Graph.svelte";
+</script>
+
+<main>
+  <Graph />
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Edit
+    <code>lib/Graph.vue</code> to start
+  </p>
+
+  <p class="read-the-docs">
+    this is an example application to run depict in Vue.
+  </p>
+</main>
+
+<style>
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ examples/svelte-graph/src/lib/Graph.svelte
@@ -0,0 +1,11 @@
+<div class="graph"></div>
+
+<style>
+  .graph {
+    position: relative;
+    width: 600px;
+    height: 400px;
+    border: 1px solid #333;
+    border-radius: 20px;
+  }
+</style>
