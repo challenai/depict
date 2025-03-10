6cb458ce0f8004bc43da1fec681e63f4e4d1eb9e
ChallenAi <fengyiai@gmail.com>
2025-03-10 21:07:03 +0000

    examples: update vanilla graph example, add a new nonworker version graph example

---
+++ examples/vanilla/graph/foo.js
@@ -0,0 +1,47 @@
+import { rectangle } from "impressionist";
+import { counterState } from "./state";
+
+export const fooGraph = [
+  {
+    x: 64,
+    y: 36,
+    texts: [
+      {
+        x: -28,
+        y: 4,
+        content: "",
+        opts: {
+          fill: "#888",
+          font: "16px san-serf",
+        }
+      }
+    ],
+    update() {
+      this.texts[0].content = `react count: ${counterState.count}, speed level: ${counterState.count % 10}`;
+    },
+  },
+  {
+    x: 198,
+    y: 181,
+    shapes: [
+      {
+        path: rectangle.basic(0, 0, 96, 72),
+        opts: {
+          stroke: "#666",
+          fill: "#333",
+        }
+      },
+    ],
+    contain(x, y) {
+      return x > -48 && x < 48 && y > -36 && y < 36;
+    },
+    onMouseenter(render) {
+      this.shapes[0].opts.fill = "#222";
+      render();
+    },
+    onMouseleave(render) {
+      this.shapes[0].opts.fill = "#333";
+      render();
+    },
+  },
+];
---
+++ examples/vanilla/graph/index.js
@@ -0,0 +1,21 @@
+import { Graph } from "@pattaya/depict/graph";
+import { textGraph } from "./text";
+import { fooGraph } from "./foo";
+import { barGraph } from "./bar";
+
+const p = new Graph();
+
+console.log(p)
+export { counterState } from "./state";
+export const graph = new Graph();
+
+graph.onReady(() => {
+  // first layer: text
+  graph.updateQueue(0, textGraph);
+  // second layer: foo
+  graph.updateQueue(1, fooGraph);
+  graph.updateLayerOptions(1, { dynamic: false, update: true });
+  // third layer: bar
+  graph.updateQueue(2, barGraph);
+  graph.updateLayerOptions(2, { dynamic: true });
+});
